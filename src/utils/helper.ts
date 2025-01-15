30bc370f165e5aac44e61e277a447d5f75553d66
codewithashim <codewithashim@gmail.com>
2025-01-16 02:29:20 +0600

    config is removed

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -3,17 +3,4 @@ export const dateFormatter = new Intl.DateTimeFormat('en-US', {
     month: 'long',
     day: 'numeric',
   });
-
-
-  export const config = {
-    name: 'Ashim Rudra Paul',
-    subtitle: 'Software Engineer',
-    birthday: '15th Jun 2002',
-    githubUsername: 'codewithashim',
-    notionBlogTableId: '17c642fed7d780f59c19d4196f0c89ba',
-    // notionAchievementTableId: '3e46de9f7eb847e596170b59c16c6fec',
-    // notionProjectTableId: 'd3f8deb550f94b7b9b43461e266a0621',
-  };
-
-
   
